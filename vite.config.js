12b626a2d5796b127a02e9ce6aa42dcdcec4b283
Project Manager <test@example.com>
2025-12-11 16:35:52 +0900

    feat: GitHub Pages 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  base: '/gold-stock-coin-live/',
   server: {
     host: "::",
     port: 8080,
